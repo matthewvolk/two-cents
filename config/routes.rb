01d95a9766a9d50f2b3ad9eeb595edb4e00f5928
Matthew Volk <volkmattj@gmail.com>
2017-07-13 06:18:58 +0000

    added custom google fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#home' # root is like homepage, so we're saying the homepage needs to point to the pages controller, home method
 end
+
+# routes file is the first file opened when someone comes to the site, rails says "okay, the root (the homepage) points to the pages controller, and the home action in there"
+# then ruby says "okay, there's a home action in there, I need to grab a corresponding view file, it gathers that code, and sends to users browser"
