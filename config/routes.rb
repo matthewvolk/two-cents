441a9c1bccd46ae67945d1ea96f9af1445f707fe
Matthew Volk <volkmattj@gmail.com>
2017-07-21 00:33:31 +0000

    cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
-Rails.application.routes.draw do
-  root to: 'pages#home' # root is like homepage, so we're saying the homepage needs to point to the pages controller, home method
-  get 'about', to: 'pages#about'
-  resources :contacts #creates a RESTful api for http web functionality
-  get 'contact-us', to: 'contacts#new'
-end
-
 # routes file is the first file opened when someone comes to the site, rails says "okay, the root (the homepage) points to the pages controller, and the home action in there"
 # then ruby says "okay, there's a home action in there, I need to grab a corresponding view file, it gathers that code, and sends to users browser"
+
+Rails.application.routes.draw do
+  root to: 'pages#home'                   # root is like homepage, so we're saying the homepage needs to point to the pages controller, home method
+  get 'about', to: 'pages#about'
+  resources :contacts, only: :create      #creates a RESTful api for http web functionality
+  get 'contact', to: 'contacts#new', as: 'new_contact'
+end
+
+ 
