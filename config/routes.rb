5cebe8510db4a27c2059187a8520f2eb3fc206fe
Matthew Volk <volkmattj@gmail.com>
2017-08-02 04:17:25 +0000

    installed devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 # then ruby says "okay, there's a home action in there, I need to grab a corresponding view file, it gathers that code, and sends to users browser"
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'                                   # root is like homepage, so we're saying the homepage needs to point to the pages controller, home method
   get 'about', to: 'pages#about'
   resources :contacts, only: :create                      #creates a RESTful api for http web functionality
