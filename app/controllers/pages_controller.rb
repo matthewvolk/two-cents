719e712507c98fe4d2e8f45e0105e47b6aff05aa
Matthew Volk <volkmattj@gmail.com>
2017-08-24 06:19:37 +0000

    added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
-class PagesController < ApplicationController #AppCont is a pre-written rails based class
-  def home # <- functions inside of a class are called methods
+class PagesController < ApplicationController           #AppCont is a pre-written rails based class
+
+  # GET Request for "/", which is our homepage
+  def home                                              # <- functions inside of a class are called methods
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
   def about
