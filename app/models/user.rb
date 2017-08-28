ea524b9b1ceba9b0b8337b6c95393b4d674eeb9c
Matthew Volk <volkmattj@gmail.com>
2017-08-28 03:28:22 +0000

    added comments explaining  Stripe functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripe_card_token
+  # If Pro user passes validations (email, password, etc.)
+  # then call Stripe and tell Stripe to set up a subscription
+  # upon charging the customers card via card token (bc we don't store cc info)
+  # Stripe will respond back with customer data, and we store customer.id as the
+  # customer token and then save the user using the Devise gem. 
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
