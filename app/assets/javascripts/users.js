0a9f3a9442407ff7f4d957c88bb55a27708ad775
Matthew Volk <volkmattj@gmail.com>
2017-08-28 03:19:55 +0000

    added server side code for creating subscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,84 +1,76 @@
 /* global $, Stripe */
 
+//Document ready.
+
 $(document).on('turbolinks:load', function(){
   
-  // var declaration
-  var theForm = $('#pro_form'),
-      submitBtn = $('form-signup-btn');
+  var theForm = $('#pro_form');
+  var submitBtn = $('#form-signup-btn');
   
-  // Set Stripe public key
+  //Set Stripe public key.
   Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
   
-  // When user clicks submit, 
+  //When user clicks form submit btn,
   submitBtn.click(function(event){
-    // prevent default submission path
+    
+    //prevent default submission behavior.
     event.preventDefault();
+    
     submitBtn.val("Processing...").prop('disabled', true);
     
-  
-  // Collect credit card fields
-  var ccNum = $('#card_number').val(),
-      cvcNum = $('#card_code').val(),
-      expMonth = $('#card_month').val(),
-      expYear = $('#card_year').val();
-      
-      
-      
-  // BEGIN ERROR HANDLING
-  var error = false;
-  
-  // Validate card number
-  if (!Stripe.card.validateCardNumber(ccNum)) {
-    error = true;
-    alert("The credit card number appears to be invalid.");
-  }
-  
-  // Validate CVC
-  if (!Stripe.card.validateCardNumber(cvcNum)) {
-    error = true;
-    alert("The CVC number on the back of your card appears to be invalid.");
-  }
-  
-  // Validate expiration date
-  if (!Stripe.card.validateExpiry(expMonth, expYear)) {
-    error = true;
-    alert("The expiration date appears to be invalid.");
-  }
-  // END ERROR HANDLING
-  
-  
-  
-  if (error) {
-    // If there are card errors, do not send to Stripe
-    submitBtn.prop('disabled', false).val("Sign Up");
-  } else {
-    // Send card info to stripe
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
-  }
-  
-  return false;
+    //Collect the credit card fields.
+    
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+        
+    //Use Stripe JS library to check for card errors.
+    var error = false;
+    
+    //Validate card number.
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+    
+    //Validate CVC number.
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+    
+    //Validate expiration date.
+    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+    
+    if (error) {
+      //If there are card errors, don't send to Stripe.
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else {
+      //Send the card info to Stripe.
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
   });
   
-
-    
-  // Stripe will return a card token
+  //Stripe will return a card token.
   function stripeResponseHandler(status, response) {
     
-    // Get the token from the response
+    //Get the token from the response.
     var token = response.id;
     
-    // Inject card token as hidden field into form
+    //Inject the card token in a hidden field.
     theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
     
-    // Submit form to our Rails app
+    //Submit form to our Rails app.
     theForm.get(0).submit();
   }
-  
 });
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-# routes file is the first file opened when someone comes to the site, rails says "okay, the root (the homepage) points to the pages controller, and the home action in there"
+# Routes.rb is the first file opened when someone comes to the site, rails says "okay, the root (the homepage) points to the pages controller, and the home action in there"
 # then ruby says "okay, there's a home action in there, I need to grab a corresponding view file, it gathers that code, and sends to users browser"
 
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'                                   # root is like homepage, so we're saying the homepage needs to point to the pages controller, home method
+  root to: 'pages#home'
+  devise_for :users, controllers: { registrations: "users/registrations" }
   get 'about', to: 'pages#about'
-  resources :contacts, only: :create                      #creates a RESTful api for http web functionality
+  resources :contacts, only: :create
   get 'contact', to: 'contacts#new', as: 'new_contact'
 end
